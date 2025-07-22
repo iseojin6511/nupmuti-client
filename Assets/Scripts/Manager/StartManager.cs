390cead86467320a67a4307dcaf145e17a84c526
iseojin6511 <seojin7764@gmail.com>
2025-07-22 16:06:57 +0900

    ++

--- Assets/Scripts/Manager/StartManager.cs
+++ Assets/Scripts/Manager/StartManager.cs
@@ -14,19 +14,26 @@ public class StartManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        NetworkManager.Instance.RegisterHandler<ResponsePacketData.StartRound>(OnStartRound);
         NetworkManager.Instance.RegisterHandler<ResponsePacketData.FirstRoundRules>(OnFirstRoundRules);
         NetworkManager.Instance.RegisterHandler<ResponsePacketData.YourCard>(OnYourCard);
         NetworkManager.Instance.RegisterHandler<ResponsePacketData.YourRank>(OnYourRank);
         NetworkManager.Instance.RegisterHandler<ResponsePacketData.YourOrder>(OnYourOrder);
     }
 
-    private void OnFirstRoundRules(ResponsePacketData.FirstRoundRules data)
+    private void OnStartRound(ResponsePacketData.StartRound data)
     {
         if (gameStartText != null)
         {
             // 1초 동안 알파값을 0으로 페이드아웃
             gameStartText.DOFade(0f, 1f);
         }
+    }
+
+    private void OnFirstRoundRules(ResponsePacketData.FirstRoundRules data)
+    {
+        Debug.Log("FirstRoundRules: " + data.message);
+        ShowMessage(data.message);
     }
 
     private void OnYourCard(ResponsePacketData.YourCard data)
